e9c4ddd8528864b2f13167cf921b46d10ed8d2f3
Ballarja <JamesBallard87@hotmail.com>
2023-11-16 09:00:09 -0600

    Completed fourth Javascript coding exercise

---
+++ Day1/Fourth_Challenge.js
@@ -0,0 +1,7 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+
+let a = Number(readLine())
+
+console.log(a+1);
